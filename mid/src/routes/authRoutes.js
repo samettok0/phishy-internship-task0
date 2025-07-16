e0bafec5869e34f68bff95baceb9679092bee4e8
samettok0 <99971700+samettok0@users.noreply.github.com>
2025-07-16 17:26:22 +0300

    login endpoint covered with JWT

--- mid/src/routes/authRoutes.js
+++ mid/src/routes/authRoutes.js
@@ -30,6 +30,33 @@ router.post('/register', (req, res) => {
 })
 // login a new user endpoint at /auth/login
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?'); // fetches the whole row where username == ?
+        const user = getUser.get(username); // get the 'username' row by calling get from getUser 
+
+        // if user not found = its null
+        if (!user) {
+            return res.status(404).send({ // return response 404 code error
+                message: "User not found"
+            });
+        }
+
+        const ispwValid = bcrypt.compareSync(password, user.password); // turn pw to hashed value and, compare that hash with stored hash in db
+        if (ispwValid === false) { // if pw is not matched
+            return res.status(401).send({ // return 401 coded error 
+                message: "Invalid password!"
+            })
+        }
+
+        // if we didnt return anything from above code that means we are in
+        const token = jwt.sign({ id: user.id }, process.env.JWT_secret, { expiresIn: '24h' })
+        res.json({ token });
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(503);
+    }
 
 })
 
