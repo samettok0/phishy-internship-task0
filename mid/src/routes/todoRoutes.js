5c21bd606da4deca4d1c139736fad38ce99d6871
samet tok <99971700+samettok0@users.noreply.github.com>
2025-07-18 16:54:02 +0300

    Post todos and update todos endpoints added

--- mid/src/routes/todoRoutes.js
+++ mid/src/routes/todoRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import db from '../db.js' 
+import db from '../db.js'
 
 const router = express.Router();
 
@@ -12,16 +12,27 @@ router.get('/', (req, res) => {
 
 // create a new todo
 router.post('/', (req, res) => {
+    const { task } = req.body;
+    const insertTodo = db.prepare('INSERT INTO todos (user_id, task) VALUES (?,?)')
+    const result = insertTodo.run(req.userId, task);
 
+    res.json({ id: result.lastInsertRowid, task, completed: 0 });
 })
 
 // update a todo
 router.put('/:id', (req, res) => {
+    const { completed } = req.body;
+    const { id } = req.params // params takes you to access id via /:id
+    const { page } = req.query
 
+    const updatedTodo = db.prepare("UPDATE todos SET completed = ? WHERE id = ?")
+
+    updatedTodo.run(completed, id)
+    res.json({ message: "Todo completed" })
 })
 
 // delete a todo
-router.delete('/:id', (req, res) => { 
+router.delete('/:id', (req, res) => {
 
 })
 
